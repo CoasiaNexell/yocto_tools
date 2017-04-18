740398b9d366b90c78c7e15928051580b195f668
suker <suker@nexell.co.kr>
2017-04-18 17:45:59 +0900

    tools: s5p6818-kick-st generator append
    The generator script update for s5p6818-kick-st board.
    Ths scripts will be create about s5p6818-kick-st bbappend files.
    
    Change-Id: Ib667968e8f2db8a1bfbec9de9011c355d5465746
    Signed-off-by: suker <suker@nexell.co.kr>

--- bbappend-files/remove_bbappend.py
+++ bbappend-files/remove_bbappend.py
@@ -27,6 +27,7 @@ HASH_RECIPENAME_PATH = {
     's5p4418-smart-voice-bl1.bbappend':     ['ON',['/bl1/bl1-s5p4418',R_BL1,'/bl1-s5p4418'], []],
     's5p6818-artik710-raptor-bl1.bbappend': ['ON',['/bl1/bl1-s5p6818',R_BL1,'/bl1-s5p6818'], []],
     's5p6818-avn-ref-bl1.bbappend':         ['ON',['/bl1/bl1-s5p6818',R_BL1,'/bl1-s5p6818'], []],
+    's5p6818-kick-st-bl1.bbappend':         ['ON',['/bl1/bl1-s5p6818',R_BL1,'/bl1-s5p6818'], []],
 
     's5p4418-avn-ref-bl2.bbappend':         ['ON',['/secure/bl2-s5p4418',R_BL2,'/secure-s5p4418'], []],
     's5p4418-navi-ref-bl2.bbappend':        ['ON',['/secure/bl2-s5p4418',R_BL2,'/secure-s5p4418'], []],
@@ -48,9 +49,10 @@ HASH_RECIPENAME_PATH = {
 
     's5p4418-avn-ref-uboot_%.bbappend':         ['ON',['/u-boot/u-boot-2016.01',R_UBOOT,'/u-boot-2016.01'],[]],
     's5p4418-navi-ref-uboot_%.bbappend':        ['ON',['/u-boot/u-boot-2016.01',R_UBOOT,'/u-boot-2016.01'],[]],
+    's5p4418-smart-voice-uboot_%.bbappend':     ['ON',['/u-boot/u-boot-2016.01',R_UBOOT,'/u-boot-2016.01'],[]],
     's5p6818-artik710-raptor-uboot_%.bbappend': ['ON',['/u-boot/u-boot-2016.01',R_UBOOT,'/u-boot-2016.01'],[]],
     's5p6818-avn-ref-uboot_%.bbappend':         ['ON',['/u-boot/u-boot-2016.01',R_UBOOT,'/u-boot-2016.01'],[]],
-    's5p4418-smart-voice-uboot_%.bbappend':     ['ON',['/u-boot/u-boot-2016.01',R_UBOOT,'/u-boot-2016.01'],[]],
+    's5p6818-kick-st-uboot_%.bbappend':         ['ON',['/u-boot/u-boot-2016.01',R_UBOOT,'/u-boot-2016.01'],[]],
 
     'gst-plugins-camera_%.bbappend':    ['ON',['/library/gst-plugins-camera',R_GST_LIBS,'/gst-plugins-camera'],[]],
     'gst-plugins-renderer_%.bbappend':  ['ON',['/library/gst-plugins-renderer',R_GST_LIBS,'/gst-plugins-renderer'],[]],
@@ -66,9 +68,10 @@ HASH_RECIPENAME_PATH = {
 
     'linux-s5p4418-avn-ref_%.bbappend':         ['ON',['/kernel/kernel-4.4.x',R_KERNEL,'/kernel-4.4.x'],  []],
     'linux-s5p4418-navi-ref_%.bbappend':        ['ON',['/kernel/kernel-4.4.x',R_KERNEL,'/kernel-4.4.x'],  []],
+    'linux-s5p4418-smart-voice_%.bbappend':     ['ON',['/kernel/kernel-4.4.x',R_KERNEL,'/kernel-4.4.x'],  []],
     'linux-s5p6818-artik710-raptor_%.bbappend': ['ON',['/kernel/kernel-4.4.x',R_KERNEL,'/kernel-4.4.x'], []],
     'linux-s5p6818-avn-ref_%.bbappend':         ['ON',['/kernel/kernel-4.4.x',R_KERNEL,'/kernel-4.4.x'], []],
-    'linux-s5p4418-smart-voice_%.bbappend':     ['ON',['/kernel/kernel-4.4.x',R_KERNEL,'/kernel-4.4.x'],  []],
+    'linux-s5p6818-kick-st_%.bbappend':         ['ON',['/kernel/kernel-4.4.x',R_KERNEL,'/kernel-4.4.x'], []],
 
     'testsuite-s5p4418_%.bbappend' :            ['ON',['/apps/testsuite',R_TESTSUITE,'/testsuite'],[]],
     'testsuite-s5p6818_%.bbappend' :            ['ON',['/apps/testsuite',R_TESTSUITE,'/testsuite'],[]],
--- build.sh
+++ build.sh
@@ -38,7 +38,7 @@ NEED_KERNEL_MAKE_CLEAN=false
 META_NEXELL_PATH=`readlink -ev ${ROOT_PATH}/yocto/meta-nexell`
 GENIVI_PATH=`readlink -e ${ROOT_PATH}/yocto/GENIVI`
 
-declare -a targets=("s5p4418-avn-ref" "s5p4418-navi-ref" "s5p6818-artik710-raptor" "s5p6818-avn-ref" "s5p4418-smart-voice")
+declare -a targets=("s5p4418-avn-ref" "s5p4418-navi-ref" "s5p6818-artik710-raptor" "s5p6818-avn-ref" "s5p4418-smart-voice" "s5p6818-kick-st")
 declare -a imagetypes=("qt" "tiny" "sato" "tinyui" "genivi" "smartvoice")
 
 function check_usage()
