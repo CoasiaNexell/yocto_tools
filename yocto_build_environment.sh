1b38b2f9b2156821dbe443749c6994b3a459f2c1
Lee Geunjeong <kjlee@nexell.co.kr>
2018-10-29 15:21:24 +0900

    tools: Update environment script
    This patch adds xutil-dev tools in environment script.
    
    Change-Id: I438f2ebad96b8a054992b08475be520327bb36bb
    Signed-off-by: Lee Geunjeong <kjlee@nexell.co.kr>

--- yocto_build_environment.sh
+++ yocto_build_environment.sh
@@ -10,7 +10,7 @@ sudo apt-get install libsdl1.2-dev xter
 #sudo apt-get install make xsltproc docbook-utils fop dblatex xmlto
 
 #SDK Installer Extras: Packages needed if you are going to be using the the standard or extensible SDK:
-sudo apt-get install autoconf automake libtool libglib2.0-dev libarchive-dev
+sudo apt-get install xutils-dev autoconf automake libtool libglib2.0-dev libarchive-dev
 
 #OpenEmbedded Self-Test (oe-selftest): Packages needed if you are going to run oe-selftest:
 sudo apt-get install python-git
